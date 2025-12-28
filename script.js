ab4a6cf81fade51617be2ab25902066f059a76d0
Antonino <tony.spataro.89@gmail.com>
2025-12-28 17:18:40 +0100

    Codice pulito

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-// 1.  Recupero le categorie e creo i pulsanti
 fetch('https://fakestoreapi.com/products/categories')
     .then((informazioni) => {
-        // console.log(informazioni);
         let result = informazioni.json();
         return result
     })
     .then(categories => {
-        // console.log(categories);
         let nav = document.querySelector('nav')
 
         categories.forEach(category => {
@@ -17,15 +14,10 @@ fetch('https://fakestoreapi.com/products/categories')
         });
     }).catch(err => console.error('Errore nel caricamento delle categorie', err));
 
-
-
-// 2.  Scarico i prodotti dalla categoria selezionata
-
 function products(category) {
     fetch(`https://fakestoreapi.com/products/category/${category}`)
         .then(informazioni => informazioni.json())
         .then(products => {
-            // console.log(products)
             let main = document.querySelector('main');
             main.replaceChildren();
 
